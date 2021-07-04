8b08e99b6b59eb73177649c82830effc64f7d52b
Jose L Alvarez Pulido <jlalvarezpulido@gmail.com>
2021-07-03 17:20:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-let Mocha = document.getElementById("Mocha");
-let Latte = document.getElementById("Latte");
-let n = 5;
-let button0 = document.getElementById("Not");
-let button1 = document.getElementById("Reset");
+var Mocha = document.getElementById("Mocha");
+var Latte = document.getElementById("Latte");
+var n = 5;
+var button0 = document.getElementById("Not");
+var button1 = document.getElementById("Reset");
 button0.innerHTML = "Not";
 button1.innerHTML = "Reset";
 button0.onclick = function(){

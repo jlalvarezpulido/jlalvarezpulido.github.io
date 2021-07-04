caa722213c28c7db1bf8666244a303c97f7455fa
Jose L Alvarez Pulido <jlalvarezpulido@gmail.com>
2021-07-03 17:14:46 -0700

    Buttons

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 let Mocha = document.getElementById("Mocha");
 let Latte = document.getElementById("Latte");
 let n = 5;
-// Mocha.innerHTML = n;
-// Latte.innerHTML = "Latte";
-
-
-
-
+let button0 = document.getElementById("Not");
+let button1 = document.getElementById("Reset");
+button0.innerHTML = "Not";
+button1.innerHTML = "Reset";
+button0.onclick = function(){
+     Mocha.innerHTML = "Not Mocha";
+     Latte.innerHTML = "Not Latte";
+}
+button1.onclick = function(){
+    Mocha.innerHTML = "Esspresso Steamed milk and Syrup"; 
+    Latte.innerHTML = "Milky Froathy Esspresso";
+}
